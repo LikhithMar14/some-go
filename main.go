3007c65165e1356ae31bf57a552da530171b5a62
2005LIKHITH <likhithcvsrl@gmail.com>
2025-05-06 18:19:09 +0530

    kuch padhai

--- main.go
+++ main.go
@@ -5,42 +5,74 @@ import (
 	"fmt"
 	"log"
 )
+type Truck interface{
+	LoadCargo() error
+	UnloadCargo() error
 
-type Truck struct{
+}
+type NormalTruck struct{
 	id string
 	cargo int
 }
-
+type ElectricTruck struct{
+	id string
+	cargo int
+	battery float64
+}
 var (
 	ErrorNotImplemented = errors.New("not implemented")
 	ErrorTruckNotFound = errors.New("truck not found")
 )
 func processTruck(truck Truck) error {
-	fmt.Printf("Processing Truck %s\n",truck.id)
+	fmt.Printf("Processing truck: %+v \n",truck)
 	if err := truck.LoadCargo(); err != nil{
 		return fmt.Errorf("error loading cargo: %w",err)
 	}
+
 	return nil
 }
 
-func(t *Truck) LoadCargo() error{
+func(t *NormalTruck) LoadCargo() error{
+	t.cargo += 100
 	return nil
 }
-func(t *Truck) UnloadCargo() error{
+func(t *NormalTruck) UnloadCargo() error{
+	t.cargo -= 100
+	return nil
+}
+func(t *ElectricTruck) LoadCargo() error{
+	t.cargo += 100
+	t.battery -= 10
+	return nil
+}
+func(t *ElectricTruck) UnloadCargo() error{
+	t.cargo -= 100
 	return nil
 }
 func main(){
-	trucks := []Truck{
-		{id : "Truck-1"},
-		{id : "Truck-2"},
-		{id : "Truck-3"},
-	}
-	for _,truck := range trucks{
-		fmt.Printf("Processing truck: %s\n",truck.id)
-		err := processTruck(truck); if err != nil{
-			log.Fatalf("Error processing truck: %v\n",err)
-		}
-	}
+	nt := NormalTruck{id : "Truck-1",cargo : 100}
+	et := ElectricTruck{id : "eTruck-1",battery : 100,cargo : 100}
+	err := processTruck(&nt)
+	// can be replaced with any interfce{} == any
+	person := make(map[string]interface{},0)
+	person["name"] = "John"
+	person["age"] = 20
+	person["city"] = "New York"
 	
+	age, ok := person["age"].(int)
+	if !ok{
+		log.Fatalf("Age does not exist")
+	}
+	log.Println(age)
+
+	if err != nil{
+		log.Fatalf("Error processing truck: %v\n",err)
+	}
+	err = processTruck(&et)
+	if err != nil{
+		log.Fatalf("Error processing truck: %v\n",err)
+	}
+	log.Println(&nt)
+	log.Println(&et)
 	
 }
